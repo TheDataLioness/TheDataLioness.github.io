ed0e50f30195b5d2da79a24d519629cb3d6a06f6
DataLioness | Damy op den Kamp <datalionmcpe@gmail.com>
2025-01-08 14:40:28 +0100

    [Fix] Hero Section video looping

--- src/components/hero/HeroSection.js
+++ src/components/hero/HeroSection.js
@@ -4,7 +4,7 @@ import "./HeroSection.scss";
 export default function HeroSection() {
   return (
     <div className="hero-container">
-      <iframe src="https://www.youtube.com/embed/AKISXZogN5w?mute=1&autoplay=1&loop=1" id="herovideo" />
+      <iframe src="https://www.youtube.com/embed/AKISXZogN5w?&autoplay=1&mute=1&loop=1&rel=0&showinfo=0&color=white&iv_load_policy=3&playlist=AKISXZogN5w" id="herovideo" />
     </div>
   );
 }
