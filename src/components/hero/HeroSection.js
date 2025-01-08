f4dda29ad5f6486aa8195dba5bf2aefb3d465d62
Damy op den Kamp <datalionmcpe@gmail.com>
2025-01-08 16:50:21 +0100

    [Fix] Browser compatibility

--- src/components/hero/HeroSection.js
+++ src/components/hero/HeroSection.js
@@ -7,6 +7,7 @@ export default function HeroSection() {
       <iframe
         src="https://www.youtube.com/embed/AKISXZogN5w?&autoplay=1&mute=1&loop=1&rel=0&showinfo=0&color=white&iv_load_policy=3&playlist=AKISXZogN5w"
         id="herovideo"
+        title="heroVideo"
       />
     </div>
   );
